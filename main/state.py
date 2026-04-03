10d3f9fba197f7bab7fe8c33caf503ec70ca4631
Akshata Vyas <akshatavyas01@gmail.com>
2026-04-03 16:41:38 +0530

    Added Fact as BaseModel for better output control.

--- main/state.py
+++ main/state.py
@@ -1,9 +1,15 @@
-from typing import TypedDict
+from typing import TypedDict, Literal
+from pydantic import BaseModel, Field
 
+class Fact(BaseModel):
+    argument: str=Field(description="The argument that needs to be classified.")
+    classification:Literal["True","False","Needs Verification"]=Field(description="The arguments need to be classified into literals 'True','False' and 'Needs Verification'.")
+    reason:str=Field(description="A secific reason  why a certain literal was assigned.")
+    
 class agent_state(TypedDict, total=False):
     topic:str
-    pro_agruments:list[str]
+    pro_arguments:list[str]
     con_arguments:list[str]
     deb_round:int
-    facts:list[dict]
+    facts:Fact
     verdict:str
