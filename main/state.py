8a1e435c616f6d7d81f0d0869f2070f3d8246295
Akshata Vyas <akshatavyas01@gmail.com>
2026-04-06 17:17:27 +0530

    Removed reason from Facts as it was not yielding the exact results.

--- main/state.py
+++ main/state.py
@@ -4,8 +4,7 @@ from pydantic import BaseModel, Field
 class Fact(BaseModel):
     argument: str=Field(description="The argument that needs to be classified.")
     classification:Literal["True","False","Needs Verification"]=Field(description="The arguments need to be classified into literals 'True','False' and 'Needs Verification'.")
-    reason:str=Field(description="A secific reason  why a certain literal was assigned.")
-    
+
 class agent_state(TypedDict, total=False):
     topic:str
     pro_arguments:list[str]
